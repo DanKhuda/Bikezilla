8a73c8e5d1902e0af0d1a72387e1f5d4fd3d4b8f
DanKhuda <dan.network.17@gmail.com>
2019-07-04 23:15:14 +0300

    pug added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 
 module.exports = {
     entry: './src/index.js',
@@ -15,10 +17,18 @@ module.exports = {
                     fallback: 'style-loader',
                     use: ['css-loader', 'sass-loader']
                 })
-            }
+            },
+            {
+                test: /\.pug$/,
+                use: ["pug-loader"]
+            },
         ]
     },
     plugins: [
-        new ExtractTextPlugin('style.css')
+        new ExtractTextPlugin('style.css'),
+        new HtmlWebpackPlugin({
+            filename: "test.html",
+            template: "./src/index.pug"
+        })
     ]
 };
